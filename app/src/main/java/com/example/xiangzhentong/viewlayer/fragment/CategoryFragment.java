e3fe2761d8b66fb4d31d0373b4c1ef85e2132e05
luckyiyiyi <2562938569@qq.com>
2020-03-14 11:09:18 +0800

    分类界面以及跳转（缺图片）

--- app/src/main/java/com/example/xiangzhentong/viewlayer/fragment/CategoryFragment.java
+++ app/src/main/java/com/example/xiangzhentong/viewlayer/fragment/CategoryFragment.java
@@ -1,6 +1,7 @@
 package com.example.xiangzhentong.viewlayer.fragment;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,8 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.xiangzhentong.R;
+import com.example.xiangzhentong.viewlayer.activity.BianminxinxiActivity;
+import com.example.xiangzhentong.viewlayer.activity.GerenjianzhiActivity;
+import com.example.xiangzhentong.viewlayer.activity.GongyiguanggaoActivity;
+import com.example.xiangzhentong.viewlayer.activity.ShangjiaguanggaoActivity;
+import com.example.xiangzhentong.viewlayer.activity.ZhenfuzhenceActivity;
 
-public class CategoryFragment extends Fragment {
+public class CategoryFragment extends Fragment{
 
     private Activity activity;
     @Override
@@ -17,7 +23,49 @@ public class CategoryFragment extends Fragment {
                              Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_category,container,false);
         activity = getActivity();
+        initview(view);
         return view;
+    }
+    public void initview(View view){
+        View zfzcview = view.findViewById(R.id.zhengfuzhengce);
+        zfzcview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent zfzcintent = new Intent(activity, ZhenfuzhenceActivity.class);
+                startActivity(zfzcintent);
+            }
+        });
+        View bmxxview = view.findViewById(R.id.bianminxinxi);
+        bmxxview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bmxxintent = new Intent(activity, BianminxinxiActivity.class);
+                startActivity(bmxxintent);
+            }
+        });
+        View grjzview = view.findViewById(R.id.gerenjianzhi);
+        grjzview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent grjzintent = new Intent(activity, GerenjianzhiActivity.class);
+                startActivity(grjzintent);
+            }
+        });
+        View sjggview = view.findViewById(R.id.shangjiaguanggao);
+        sjggview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sjggintent = new Intent(activity, ShangjiaguanggaoActivity.class);
+                startActivity(sjggintent);
+            }
+        });
+        View gyggview = view.findViewById(R.id.gongyiguanggao);
+        gyggview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gyggintent = new Intent(activity, GongyiguanggaoActivity.class);
+                startActivity(gyggintent);            }
+        });
     }
 
 }
