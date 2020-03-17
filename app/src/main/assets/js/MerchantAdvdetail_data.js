f4fc51a790e45ed1ee78cfd60f07ede93664d060
luckyiyiyi <2562938569@qq.com>
2020-03-17 18:42:30 +0800

    商家广告广告详情页

--- app/src/main/assets/js/MerchantAdvdetail_data.js
+++ app/src/main/assets/js/MerchantAdvdetail_data.js
@@ -1,5 +1,4 @@
 let MerchantAdvdetail_data = [
-
   {
     Title:'苏州周边，流动补胎，汽修',
     Image:'https://s2.ax1x.com/2019/09/05/nuilVK.jpg',
@@ -36,6 +35,28 @@ let MerchantAdvdetail_data = [
     Text: '免费上门免贴：玻璃贴膜/防爆膜/隔热膜/磨砂膜/银行防爆膜/淋浴房玻璃防爆膜/办公室贴膜/大厦贴膜/单向透视膜/幕墙贴膜/医院贴膜/学校贴膜 专业承接全国各地大小建筑玻璃贴膜。\n 玻璃贴膜的好处：\n 1、隔紫外线：紫外线中的长波能穿透玻璃，甚至9英尺的水。玻璃贴膜能隔断95%以上的紫外线。大大降低室内因紫外线引起的家具褪色，延长家具，地毯等的使用寿命。当然也能让您在享受大自然阳光的沐浴时不为紫外线所伤害。\n 2、安全与防爆：普通玻璃在贴膜后，其强度将增强4倍以上，大大增强了玻璃的抗冲击能力，即使遇到地震和台风等强力冲击而发生破裂，飞刀般的碎片也不会四下飞溅。而能将玻璃碎片牢牢地粘在膜上，从而不会对人员造成伤害。\n 3、保留隐私：玻璃贴膜具有优越的单向透视性，不像窗帘，不必牺牲你美丽的视野也能保留隐私，还您自由自在的生活空间。\n 4、节省能源，省电：炎热的夏天，不开空调房间的温度可能是34度，但在玻璃窗上安装了玻璃膜的房间可能只有30度，它高达30%-80%的隔热率为您节省大量的电费。而玻璃膜除了隔热效果外，在冬日更可将20~30%的热能反射回屋内，使屋内更温暖舒适。防眩光，保持眼睛舒适：玻璃膜中有金属反射层，在受到强光的照射时，会将部分光线反射，从而保持室内光线的柔和。\n 5、丰富建筑物的色彩：玻璃贴膜有丰富的色彩，可起到装饰建筑物的作用。联系我时，请说是在58同城看到的，谢谢！',
     postId: 3
   },
-
-
 ];
+
+function setDiv(item){
+      var div = '<div><div><p class="title">'
+  + item.Title
+  + '</p></div><div class="contentimg"><img class="img" src="'
+  + item.Image
+  + '"/></div><div><p class="category">类别：'
+  + item.Category
+  + '</p></div><div><p class="contact">联系人：'
+  + item.Contact
+  + '</p></div><div><p class="address">商家地址：'
+  + item.Address
+  + '</p></div><div><p class="text">'
+  + item.Text
+  + '</p></div></div><br>'
+return div
+}
+function create(){
+    var html='';
+    var i=window.sjggposition.sjggposition();
+    html+=setDiv(MerchantAdvdetail_data[i]);
+    document.getElementById('sjggxx').innerHTML=html;
+}
+window.onload=create;
--- app/src/main/java/com/example/xiangzhentong/viewlayer/activity/ShangjiaguanggaoxiangxiActivity.java
+++ app/src/main/java/com/example/xiangzhentong/viewlayer/activity/ShangjiaguanggaoxiangxiActivity.java
@@ -1,21 +1,54 @@
 package com.example.xiangzhentong.viewlayer.activity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import com.example.xiangzhentong.R;
 
 public class ShangjiaguanggaoxiangxiActivity extends AppCompatActivity {
 
+    private WebView webView;
+    private String position;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shangjiaguanggaoxiangxi);
+        //调整状态栏颜色
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//因为不是所有的系统都可以设置颜色的，在4.4以下就不可以。。有的说4.1，所以在设置的时候要检查一下系统版本是否是4.1以上
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.guanggao));
+        }
+        TextView grjztop = findViewById(R.id.fenleitoptext);
+        grjztop.setText(R.string.sjggxxtop);
+        View topbarbut = findViewById(R.id.fenleiback);
+        topbarbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(ShangjiaguanggaoxiangxiActivity.this,ShangjiaguanggaoActivity.class);
+                startActivity(intent);
+            }
+        });
         Intent intent=getIntent();
-        String position = intent.getStringExtra("sjggposition");
-        TextView sjj=findViewById(R.id.sjjj);
-        sjj.setText(position);
+        position = intent.getStringExtra("sjggposition");
+        webView = (WebView)findViewById(R.id.sjggxxweb);
+        webView.loadUrl("file:////android_asset/html/sjggxx.html");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new sjggposition(),"sjggposition");
+    }
+    class sjggposition{
+        @JavascriptInterface
+        public String sjggposition(){
+            return position;
+        }
     }
 }
